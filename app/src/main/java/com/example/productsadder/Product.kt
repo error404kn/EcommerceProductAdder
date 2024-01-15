07ba9d975edee3041edb2c7263b7b6f793331d54
error404kn <scandaluzumaki@gmail.com>
2024-01-15 20:04:56 +0400

    EcommerceProductsAdder Updated

---
+++ app/src/main/java/com/example/productsadder/Product.kt
@@ -0,0 +1,12 @@
+package com.example.productsadder
+
+data class Product(
+    val id: String,
+    val name: String,
+    val category: String,
+    val price: Float,
+    val offerPercentage: Float? = null,
+    val description: String? = null,
+    val colors: List<Int>? = null,
+    val images: List<String>
+)
